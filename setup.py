913174d1c2525423146f53596098db761f25dcce
Salim <uie32591@contiwan.com>
2022-06-05 04:35:07 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Applied data science on COVIID 19 data (Enterprise Data Science 2022) ',
+    author='Sabin Hamed Salim',
+    license='',
+)
